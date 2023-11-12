eb48387db32927ce5150acf50cc76a153c889c85
rafsanrjkhan <rjkhan104@gmail.com>
2023-11-12 10:50:05 +0600

    add title & css link

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
 $_SESSION['user'] = (isset($_GET['user']) === true) ? (int) $_GET['user'] :0;
-
-echo $_SESSION['user'];
+// echo $_SESSION['user'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Chat App</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
